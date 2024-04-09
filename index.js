7ea4b5ddd65392ecbd48ad0703083410950e1769
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2024-04-10 04:18:10 +0700

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const createFile = require("./config/file");
 // var path = require("path");
 const dbConnect = require("./config/connect");
+const userRouter = require("./routes/userRoutes");
 require("dotenv").config();
 const app = express();
 dbConnect();
@@ -14,10 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 console.log("123123");
+app.use("/api/users", userRouter);
 
-app.use("/", (req, res, next) => {
-  res.json("preasdasd");
-});
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(notFound);
