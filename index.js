41864c0e0d50246882430b2b708f5b7ea7290a0b
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2024-04-10 04:26:48 +0700

    daddd

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 console.log("123123");
-app.use("/api/users", userRouter);
-
+app.use("/api/users", (req, res, next) => {
+  res.json("123123");
+});
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(notFound);
